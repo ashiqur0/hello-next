d163cbdbde7e53b80028a8ba86b6afa117de7d95
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-16 19:12:29 +0600

    Added Contact Info To Contact Page

--- src/app/about/contact/page.jsx
+++ src/app/about/contact/page.jsx
@@ -1,9 +1,10 @@
+import Title from '@/components/Title';
 import React from 'react';
 
 const Contact = () => {
     return (
         <div>
-            
+            <Title>My Contact: 0160000000000</Title>
         </div>
     );
 };
