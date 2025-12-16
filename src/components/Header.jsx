b4c3fe95b263ee3d8aba7b4437c801e4a8025123
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-16 18:09:25 +0600

    Create Header Component To Display Page Navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header className="px-4 py-3 border-b-2 border-gray">
+                <h2 className='text-lg font-semibold'>Dev Story</h2>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
