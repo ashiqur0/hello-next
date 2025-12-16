9b6b7c65d99ca7ff27de66be4d302cb934ecfa58
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-16 18:57:53 +0600

    Added Title To About Page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,10 @@
+import Title from '@/components/Title';
 import React from 'react';
 
 const About = () => {
     return (
         <div>
-            
+            <Title>Welcome To About</Title>
         </div>
     );
 };
