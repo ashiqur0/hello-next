53ad913ad286349dd75992f2fe0e710b06ce61ca
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-17 17:53:04 +0600

    Added Loading Component To Dynamic Component While Story Loadign

--- src/app/stories/[id]/loading.jsx
+++ src/app/stories/[id]/loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const loading = () => {
     return (
         <div className='min-h-screen flex justify-center items-center text-8xl'>
-            <h2>L</h2><span className='animate-spin'>😊</span><h2>ading</h2>
+            <h2>L</h2><span className='animate-spin'>😊</span><h2>ading</h2> Stories
         </div>
     );
 };
