add12efadbf64450a6e369451ceae0bb87aaa1dc
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-17 12:12:44 +0600

    Update Button Click Functionalities, Direct User To A Specific Route Using useRouter Hook

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,22 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Banner = () => {
+
+    const router = useRouter();
+
+    const handleBtnClick = () => {
+        const password = prompt('Enter your password');
+        if (password === '1234') {
+            router.push('/dashboard');
+        }
+    }
+
     return (
         <div className='text-center space-y-5 bg-linear-60 to-sky-100 from-sky-300 text-black p-10 rounded'>
             <h2 className='text-4xl font-bold'>Welcome to Dev Story</h2>
-            <button className='px-4 py-2 bg-sky-500 text-white rounded cursor-pointer hover:bg-black duration-200'>Share-Story</button>
+            <button onClick={handleBtnClick} className='px-4 py-2 bg-sky-500 text-white rounded cursor-pointer hover:bg-black duration-200'>Share-Story</button>
         </div>
     );
 };
