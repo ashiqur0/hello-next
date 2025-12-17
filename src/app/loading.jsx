7e473f621691c1de005f3be51c7bdcf7700b7588
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-17 17:19:58 +0600

    Create loading Component

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className='min-h-screen flex justify-center items-center'>
+            <h2>L<span className='animate-spin'>😊</span>ading</h2>
+        </div>
+    );
+};
+
+export default loading;
