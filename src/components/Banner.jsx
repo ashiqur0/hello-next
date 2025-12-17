a475364e33693c6a56f23be3fd1c34bd9407f345
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-17 12:03:30 +0600

    Update Style Of Banner Component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
+        <div className='text-center space-y-5 bg-linear-60 to-sky-100 from-sky-300 text-black p-10 rounded'>
             <h2 className='text-4xl font-bold'>Welcome to Dev Story</h2>
-            <button className='px-4 py-2 bg-sky-500 text-white rounded cursor-pointer hover:bg-black duration-200 mt-5'>Share-Story</button>
+            <button className='px-4 py-2 bg-sky-500 text-white rounded cursor-pointer hover:bg-black duration-200'>Share-Story</button>
         </div>
     );
 };
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = ({children}) => {
     return (
-        <div className='bg-linear-60 from-sky-50 to-sky-200 p-3 rounded-xl text-black'>
+        <div className='bg-linear-60 from-sky-50 to-sky-200 p-3 rounded text-black'>
             <h2 className='text-5xl font-bold'>{children}</h2>
         </div>
     );
