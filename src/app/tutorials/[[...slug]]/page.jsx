3d7d8a528a826d7cd763686145d4c1c311b6d504
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-17 15:24:10 +0600

    Practice Handling All Segment And Manage Complex Route

---
+++ src/app/tutorials/[[...slug]]/page.jsx
@@ -0,0 +1,22 @@
+import Title from '@/components/Title';
+import React from 'react';
+
+const TutorialPage = async ({ params }) => {
+
+    const { slug } = await params;
+    const [technology, topic, , page_no, subject] = slug || [];
+
+    return (
+        <div>
+            <Title>{technology} tutorials</Title>
+            <h2>{topic}</h2> <hr></hr>
+
+            <div className='flex justify-between'>
+                <h3 className='font-bold'>{subject}</h3>
+                <p>Page No. {page_no}</p>
+            </div> 
+        </div>
+    );
+};
+
+export default TutorialPage;
