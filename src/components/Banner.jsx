a10fc30cf24bd411b358ec4ad1520eb1a747990f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-17 11:58:07 +0600

    Update Banner Component And Added It To Home Component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            Banner
+            <h2 className='text-4xl font-bold'>Welcome to Dev Story</h2>
+            <button className='px-4 py-2 bg-sky-500 text-white rounded cursor-pointer hover:bg-black duration-200 mt-5'>Share-Story</button>
         </div>
     );
 };
