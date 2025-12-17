427cfe5a6c900cde3868edd8bf51d66ea3d5f78f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-17 07:58:01 +0600

    Added Nested Teams Page Link To Navbar

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,15 @@
 import Title from '@/components/Title';
+import Link from 'next/link';
 import React from 'react';
 
 const About = () => {
     return (
         <div>
             <Title>Welcome To About</Title>
+
+            <nav>
+                <Link href={'/about/teams'}>Teams</Link>
+            </nav>
         </div>
     );
 };
