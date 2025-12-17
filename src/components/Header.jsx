064c185ecd8fbf960cc3e3a830659a86fa934986
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-17 07:19:13 +0600

    Added Home Link To Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <header className="px-4 py-3 border-b-2 border-gray">
-                <h2 className='text-lg font-semibold'>Dev Story</h2>
-            </header>
-        </div>
+        <header className="px-4 py-3 border-b-2 border-gray flex justify-between items-center flex-wrap">
+            <Link href='/' className='text-lg font-semibold'>Dev Story</Link>
+        </header>
     );
 };
 
