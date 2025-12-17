2318951020243ece5664eb51bb51b8a05667a0b8
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-17 06:49:43 +0600

    Added Title In Teams Component

--- src/app/about/teams/page.jsx
+++ src/app/about/teams/page.jsx
@@ -1,9 +1,10 @@
+import Title from '@/components/Title';
 import React from 'react';
 
 const Teams = () => {
     return (
         <div>
-            
+            <Title>Welcome To Teams</Title>
         </div>
     );
 };
