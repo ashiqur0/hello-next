9a84580aa346a8f46029a05d199886467fd6b5b9
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-29 22:00:40 +0600

    Update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -20,9 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Header />
         <main className='px-4 py-8'> {children}</main>
       </body>
